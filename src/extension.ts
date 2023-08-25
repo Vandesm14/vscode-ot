3178ab73cf9be71c150db619139840f2aa02492b
Shane Vandegrift <vandesm14@gmail.com>
2023-08-25 18:09:47 -0400

    messaging and ui improvements

--- src/extension.ts
+++ src/extension.ts
@@ -3,6 +3,14 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as ot from './ot';
 
+type DocumentMessage = {
+  type: 'vscode-ot';
+  max: number;
+  content: string;
+  path: string;
+  startLine: number;
+};
+
 export async function activate(context: vscode.ExtensionContext) {
   const rootFolder = vscode.workspace.workspaceFolders;
   if (!rootFolder) {
@@ -14,6 +22,14 @@ export async function activate(context: vscode.ExtensionContext) {
 
   const rootUri = rootFolder[0].uri;
   const otFolder = vscode.Uri.joinPath(rootUri, '.ot');
+
+  let message: DocumentMessage = {
+    type: 'vscode-ot',
+    max: 0,
+    content: '',
+    path: '',
+    startLine: 0,
+  };
 
   // check if .ot/ folder exists
   try {
@@ -40,15 +56,13 @@ export async function activate(context: vscode.ExtensionContext) {
     const otOps = readOTFile(document);
 
     // Post a message back to the webview to tell the slider what the new content is
-    panel?.webview.postMessage({
-      type: 'updateContent',
-      text: ot.buildOT(otOps),
-    });
 
-    panel?.webview.postMessage({
-      type: 'updateMax',
-      maxValue: otOps.length,
-    });
+    message = {
+      ...message,
+      max: otOps.length,
+      content: ot.buildOT(otOps),
+      path: document.fileName,
+    };
   });
 
   // Create a Webview panel
@@ -100,9 +114,21 @@ export async function activate(context: vscode.ExtensionContext) {
     document ? readOTFile(document).length : 0
   );
 
+  // set message to the current document
+  if (document) {
+    const otOps = readOTFile(document);
+
+    message = {
+      ...message,
+      max: otOps.length,
+      content: ot.buildOT(otOps),
+      path: document.fileName,
+    };
+  }
+
   // Handle messages from the Webview
-  panel.webview.onDidReceiveMessage((message) => {
-    if (message.type === 'sliderChange') {
+  panel.webview.onDidReceiveMessage((msg) => {
+    if (msg.type === 'sliderChange') {
       const document =
         vscode.window.activeTextEditor?.document ?? lastOpenDocument;
       if (!document) {
@@ -111,14 +137,25 @@ export async function activate(context: vscode.ExtensionContext) {
       }
 
       const otOps = readOTFile(document);
-      const opsToApply = otOps.slice(0, message.value);
+      const opsToApply = otOps.slice(0, msg.value);
       const text = ot.buildOT(opsToApply);
 
+      let lastOp = opsToApply[opsToApply.length - 1];
+      let lastIndex = 0;
+      if (lastOp?.operation === 'insert') {
+        lastIndex = lastOp.position;
+      } else if (lastOp?.operation === 'delete') {
+        lastIndex = lastOp.start;
+      }
+
+      message = {
+        ...message,
+        content: text,
+        startLine: getLineNumber(text, lastIndex),
+      };
+
       // Post a message back to the webview to tell the slider what the new content is
-      panel?.webview.postMessage({
-        type: 'updateContent',
-        text: text,
-      });
+      panel?.webview.postMessage(message);
     }
   });
 
@@ -132,10 +169,10 @@ export async function activate(context: vscode.ExtensionContext) {
 
     // After saving the operations, send a message to the webview to update the slider's max value
     const maxOps = saveToOTFile(event.document, otOps);
-    panel?.webview.postMessage({
-      type: 'updateMax',
-      maxValue: maxOps,
-    });
+
+    message = { ...message, max: maxOps };
+
+    panel?.webview.postMessage(message);
   });
 
   vscode.commands.registerCommand('vscode-ot.openSlider', () => {
@@ -207,31 +244,66 @@ export async function activate(context: vscode.ExtensionContext) {
   }
 }
 
+function getLineNumber(input: string, index: number): number {
+  if (index < 0 || index >= input.length) {
+    throw new Error('Index is out of bounds.');
+  }
+
+  const lines = input.split('\n');
+  let currentIndex = 0;
+
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i];
+    const lineLength = line.length + 1; // Include the newline character
+    if (currentIndex + lineLength > index) {
+      return i + 1; // Line numbers are 1-based
+    }
+    currentIndex += lineLength;
+  }
+
+  return -1; // Index not found
+}
+
 function getWebviewContent(maxValue: number, content: string = '') {
   return `
   <!DOCTYPE html>
   <html>
     <head>
       <style>
-        /* Add your CSS styles here */
+        #fixed {
+          position: fixed;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 50px;
+          background-color: #333;
+          color: #fff;
+          padding: 10px;
+          box-sizing: border-box;
+        }
       </style>
     </head>
     <body>
-      <input
-        type="range"
-        id="ot-slider"
-        min="1"
-        max="${maxValue}"
-        value="${maxValue}"
-      />
-      <span id="ot-value">${maxValue} / ${maxValue}</span>
+      <div id="fixed">
+        <input
+          type="range"
+          id="ot-slider"
+          min="1"
+          max="${maxValue}"
+          value="${maxValue}"
+        />
+        <span id="ot-value">${maxValue} / ${maxValue}</span>
+        <br />
+        <span id="path"></span>
+      </div>
       <br />
-      <span id="ot-content">${content}</span>
+      <pre id="ot-content">${content}</pre>
       <script>
         const maxValue = ${maxValue};
         const slider = document.getElementById('ot-slider');
         const content = document.getElementById('ot-content');
         const otValue = document.getElementById('ot-value');
+        const path = document.getElementById('path');
         const vscode = acquireVsCodeApi();
 
         slider.addEventListener('input', (event) => {
@@ -245,14 +317,19 @@ function getWebviewContent(maxValue: number, content: string = '') {
 
         window.addEventListener('message', event => {
           const message = event.data;
-          switch (message.type) {
-            case 'updateMax':
-              slider.max = message.maxValue;
-              otValue.innerText = slider.value + ' / ' + message.maxValue;
-              break;
-            case 'updateContent':
-              content.innerText = message.text;
-              break;
+          if (message.type === 'vscode-ot') {
+            console.log({message});
+            slider.max = message.max;
+            path.innerText = message.path;
+            otValue.innerText = slider.value + ' / ' + message.max;
+            content.innerText = message.content;
+
+            // scroll the window to line
+            const lineNumber = message.startLine;
+            const lineHeight = 76;
+            const scrollOffset = window.innerHeight / 2;
+            const top = (lineNumber - 1) * lineHeight - scrollOffset;
+            window.scrollTo(0, top);
           }
         });
       </script>
