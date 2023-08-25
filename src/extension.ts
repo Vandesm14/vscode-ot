f5c4f21b3028a7328f95b5496ed82a15830e6151
Shane Vandegrift <vandesm14@gmail.com>
2023-08-25 12:19:25 -0400

    update helpers

--- src/extension.ts
+++ src/extension.ts
@@ -14,6 +14,12 @@ type DeleteOp = {
   end: number;
 }
 
+// type SelectOp = {
+//   operation: 'select';
+//   start: number;
+//   end: number;
+// }
+
 type OtOp = InsertOp | DeleteOp;
 
 export function activate(context: vscode.ExtensionContext) {
@@ -32,6 +38,21 @@ export function activate(context: vscode.ExtensionContext) {
     saveToOTFile(event.document, otOps);
   });
 
+  // vscode.window.onDidChangeTextEditorSelection((event) => {
+  //   const document = event.textEditor.document;
+  //   if (document.fileName.endsWith('.ot.json')) {
+  //     return;
+  //   }
+
+  //   const selection = event.selections[0];
+  //   const otOps: OtOp[] = [{
+  //     operation: 'select',
+  //     start: selection.start.character,
+  //     end: selection.end.character,
+  //   }];
+  //   saveToOTFile(document, otOps);
+  // });
+
   // A command to rebuild the file based on OT data. It will output a <file>.ot.<ext> file
   vscode.commands.registerCommand('vscode-ot.rebuild', () => {
     const document = vscode.window.activeTextEditor?.document;
@@ -40,16 +61,7 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
-    const otPath = path.join(
-      path.dirname(document.fileName),
-      path.basename(document.fileName) + '.ot.json'
-    );
-    if (!fs.existsSync(otPath)) {
-      vscode.window.showErrorMessage('No OT file found');
-      return;
-    }
-
-    const otOps = JSON.parse(fs.readFileSync(otPath, 'utf-8')) as OtOp[];
+    const otOps = readOTFile(document);
     const text = buildOT(otOps);
     const newText = vscode.TextEdit.replace(
       new vscode.Range(0, 0, document.lineCount, 0),
@@ -68,20 +80,16 @@ export function activate(context: vscode.ExtensionContext) {
       return;
     }
 
-    const otPath = path.join(
-      path.dirname(document.fileName),
-      path.basename(document.fileName) + '.ot.json'
-    );
-    if (!fs.existsSync(otPath)) {
-      vscode.window.showErrorMessage('No OT file found');
-      return;
-    }
-
+    const otPath = otFilePath(document);
     const insertOp: InsertOp = {
       operation: "insert",
       position: 0,
       text: document.getText()
     };
+
+    if (!otPath) {
+      return;
+    }
 
     // Prefill with file contents
     fs.writeFileSync(otPath, JSON.stringify([insertOp]));
@@ -89,17 +97,57 @@ export function activate(context: vscode.ExtensionContext) {
   });
 }
 
+function otFilePath(document: vscode.TextDocument): string | null {
+  let otPath = '';
+  if (document.fileName.endsWith('.ot.json')) {
+    otPath = document.fileName;
+  } else {
+    otPath = path.join(
+      path.dirname(document.fileName),
+      path.basename(document.fileName) + '.ot.json'
+    );
+    if (!fs.existsSync(otPath)) {
+      vscode.window.showErrorMessage('No OT file found');
+      return null;
+    }
+  }
+
+  return otPath;
+}
+
+function readOTFile(document: vscode.TextDocument): OtOp[] {
+  const otPath = otFilePath(document);
+  if (!otPath) {
+    return [];
+  }
+
+  return JSON.parse(fs.readFileSync(otPath).toString()) as OtOp[];
+}
+
 function changesToOT(
   changes: readonly vscode.TextDocumentContentChangeEvent[]
 ): OtOp[] {
-  return changes.map((change) => {
-    if (change.text.length === 0) {
+  return changes.flatMap((change) => {
+    if (change.rangeLength > 0 && change.text.length > 0) {
+      // If the change replaces text, we need to delete the old text and insert the new text
+      return [{
+        operation: 'delete',
+        start: change.range.start.character,
+        end: change.range.end.character,
+      }, {
+        operation: 'insert',
+        position: change.range.start.character,
+        text: change.text,
+      }];
+    } else if (change.text.length === 0) {
+      // If the change deletes text
       return {
         operation: 'delete',
         start: change.range.start.character,
         end: change.range.end.character,
       };
     } else {
+      // If the change inserts text
       return {
         operation: 'insert',
         position: change.range.start.character,
@@ -115,7 +163,7 @@ function saveToOTFile(document: vscode.TextDocument, ops: OtOp[]) {
     path.basename(document.fileName) + '.ot.json'
   );
 
-  let existingOps = [];
+  let existingOps: OtOp[] = [];
   if (fs.existsSync(otPath)) {
     existingOps = JSON.parse(fs.readFileSync(otPath, 'utf-8'));
   }
@@ -136,4 +184,18 @@ function buildOT(ops: OtOp[]) {
 
     return text;
   }, '');
+}
+
+function equals(a: OtOp, b: OtOp) {
+  return JSON.stringify(a) === JSON.stringify(b);
+}
+
+function isOppositeOp(a: OtOp, b: OtOp) {
+  if (a.operation === 'insert' && b.operation === 'delete') {
+    return a.position >= b.start && a.position <= b.end;
+  } else if (a.operation === 'delete' && b.operation === 'insert') {
+    return b.position >= a.start && b.position <= a.end;
+  }
+
+  return false;
 }
